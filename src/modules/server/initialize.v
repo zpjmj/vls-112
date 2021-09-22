1a324c731c7d377ef16034e65d2da00939a8c769
112_zhangbush <z17767767735@163.com>
2021-09-22 20:07:09 +0800

    up

--- src/modules/server/initialize.v
+++ src/modules/server/initialize.v
@@ -11,11 +11,7 @@ fn (mut ls Vls112) initialize(id string, params string)?{
 		return err
 	}
 
-	ls.logger.info('vvvvvv',0)?
-	ls.logger.info('initialize-->',0)?
-	ls.logger.text(params,0,'\t')?
-	ls.logger.text(initialize_params.process_id,0,'\t')?
-
-	ls.logger.info('xxxxxx',0)?
+	ls.logger.info('initialize.workspaceFolders.uri-->',0)?
+	ls.logger.text(initialize_params.workspace_folders,0,'\t')?
 
 }
